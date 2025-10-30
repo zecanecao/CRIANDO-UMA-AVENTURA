8fbe14eba4d37424c80e67a32f8dc309fd4b5912
ka <kauany.do.nascimento@escola.pr.gov.br>
2025-10-30 07:35:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ const avanca = document.querySelectorAll('.btn-proximo');
 avanca.forEach(button => {
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
 })
